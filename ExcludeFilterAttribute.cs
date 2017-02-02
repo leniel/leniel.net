3af91860097290c28f420b3fce9e67f3d95b5673
Leniel Macaferi <leniel@gmail.com>
2017-02-02 00:20:10 -0200

    Added files

---
+++ ExcludeFilterAttribute.cs
@@ -0,0 +1,17 @@
+﻿public class ExcludeFilterAttribute : FilterAttribute
+{
+    private Type filterType;
+
+    public ExcludeFilterAttribute(Type filterType)
+    {
+        this.filterType = filterType;
+    }
+
+    public Type FilterType
+    {
+        get
+        {
+            return this.filterType;
+        }
+    }
+}
